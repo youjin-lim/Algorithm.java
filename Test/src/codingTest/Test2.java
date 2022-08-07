f432816064c0ae23b87ad1dca69ed8d4934b6f72
u <u@DESKTOP-A51E790>
2022-08-08 00:00:31 +0900

    test

---
+++ Test/src/codingTest/Test2.java
@@ -0,0 +1,27 @@
+package codingTest;
+
+import java.util.Scanner;
+
+//  백준 2739번
+//[문제] 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오
+//첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+//각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+public class Test2 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("테스터 케이스 갯수 입력 : ");
+		int a = sc.nextInt();
+		int num[] = new int[a*2];
+		for(int i=0; i<a ; i++) {
+			
+			System.out.println("테스터 입력 : ");
+			
+			
+		}
+		
+		
+	}
+
+}
